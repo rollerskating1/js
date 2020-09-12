d96f1bd13ff5d41d5f50c0df6ceb1f6da946be59
rollerskating1 <837202942@qq.com>
2020-09-12 15:24:14 +0800

    second

---
+++ 7-3.js
@@ -0,0 +1,24 @@
+var n = 11;
+   for(var i = 1; i <= n; i++){
+      //每层空格数量为n减i，n为总层数，i是第几层
+
+      for(var j = 1; j <= n-i; j++){
+         
+     }
+     var cn = (1 + n)/2;
+      //每层星星数量为[2*（i-）+1]个，
+       for(var k = 1; k <= 2*(i-1)+1; k++){
+          
+        }
+       
+        
+      
+
+        var num = k -1
+        var blank = " "
+        blank = blank.repeat(j)
+        var star ="*"
+        star = star.repeat(num)
+        console.log(blank+star)
+        
+   }
