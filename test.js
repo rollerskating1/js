69b8d1ffafa36c51687c1488dd45d3d9f5ed3d28
rollerskating1 <837202942@qq.com>
2020-09-15 15:50:25 +0800

    five

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
-var buf = {"8gfdvjk56461"};
-var c =9;
-var d =10;
-var a = JSON.stringify(buf);
-console.log(a)
-  
+var arr = ['2','2','.','2']
+var len = arr.length;
+var ernum = 0
+for(var i =0;i<len ;i++){
+    if(arr[i] == "."){
+       ernum = 2;
+       }
+     }
+
+     if(ernum != 2){
+        for(var b = 0;b<len;b++){
+            if(arr[b]==2){
+    ernum +=1
+    }  
+        }
+     }
+     console.log(ernum)
