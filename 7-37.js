2d5754c65ad164e4be634a024d85d12d30993c41
rollerskating1 <837202942@qq.com>
2020-10-03 19:19:42 +0800

    six

--- 7-37.js
+++ 7-37.js
@@ -1,3 +1,4 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -25,20 +26,42 @@ rl.on('line', (line) => {
 
 
 rl.on('close', () => {
+    let counts = (arr, value) => arr.reduce((a, v) => v === value ? a + 1 : a + 0, 0);
+   
+    var arr =[]
+    for(var i = 0;i<buf.length;i++){
 
-    //算法写在这里，用console.log()输出
-
-    //需要读取命令行输入的话，使用buf变量
-var arr = []
-
-    var index = buf[0].split(" ");
-    var index1 = buf[1].split(" ");
-    arr.push(index)
-    arr.push(index1)
-    // if(arr[0][0] > arr[1][1]){
-        // console.log("The winner is x: "+arr[0][0]+" + "+arr[1][1])
-    // }
-    console.log(arr[0][0])
-
+arr.push(buf[i].toString().split(" "))
+    }
+    let cishu0 = counts(arr[1],'0')
+    let cishu1 = counts(arr[1],'1')
+    if(arr[0].length ==2){
+          if(arr[0][0] != arr[0][1] && arr[0][0]<=1000 && arr[0][1]<=1000 && Number(arr[0][0])*Number(arr[0][1])>0 && Number(arr[0][0]) == parseInt(arr[0][0]) && Number(arr[0][1])== parseInt(arr[0][1])){
+          if(arr[0][0]>arr[0][1] && cishu0 != 0){
+        
+       
+        console.log("The winner is a: "+arr[0][0]+" + "+cishu0)
+    }
+  
+    if(arr[0][0]<arr[0][1] && cishu0 ==3){
+        
+       
+        console.log("The winner is a: "+arr[0][0]+" + "+cishu0)
+    }
+    if(arr[0][1]>arr[0][0] &&cishu1>0){
+        
+       
+        console.log("The winner is b: "+arr[0][1]+" + "+cishu1)
+    }
+  
+    if(arr[0][1]<arr[0][0] && cishu1 ==3){
+        
+       
+        console.log("The winner is b: "+arr[0][1]+" + "+cishu1)
+    }
+    }
+  
+    }
+  
 });
 
--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-a =1;
-let a =2;
-console.log(a)
+var str = "  A   !hm !lk."
+str = str.replace(/A/g,"a")
+
+console.log(str)
