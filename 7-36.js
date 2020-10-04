45555cbe75493a25fc7e00b0f647e4d8da9ef970
rollerskating1 <837202942@qq.com>
2020-10-05 00:43:53 +0800

    nine

--- 7-36.js
+++ 7-36.js
@@ -27,35 +27,84 @@ rl.on('line', (line) => {
 rl.on('close', () => {
             var b = buf[0].toString().split(" ")
             var n = b[1]
-            // var str = b[0]
+            var th = b[0]
             var daoarr = []
-            var same = true
+            var dada = []
             var cn = (n/2).toFixed()
+
+            if(n<=100 && n>0 && n >0 &&n == parseInt(n)){
+   //打印右边
            str = buf.slice(1,n+1).map(v=>{
                 v = v.split('')
-            
                return v
            })
-            // console.log(str.join("\n").replace(/,/g,'').replace(/@/g,str))
-        //    console.log(str)
+          
            for(var i = str.length-1;i>=0;i--){
-            //   console.log(str[i].join().replace(/,/g,''))
               daoarr.push(str[i])
            }
+          
            for(var h = 0;h<daoarr.length;h++){
                for(var i = 0, j = n-1 ;i<cn-1,j>cn-1;i++,j--){
-                if(str[h][i]==str[h][j]){
-                  same = false
-                    break;
-                }
+                daoarr[h][j] = daoarr[h][i]
            }
+        }
+
+//打印左边
+           str1 = buf.slice(1,n+1).map(v=>{
+            v = v.split('')
+           return v
+       })
+           for(var i = str1.length-1;i>=0;i--){
+            dada.push(str1[i])
+         }
+         for(var h = 0;h<daoarr.length;h++){
+            for(var i = 0, j = n-1 ;i<cn-1,j>cn-1;i++,j--){
+             daoarr[h][i] = dada[h][j]
+        }
+    }
+
+    //原版
+    str2 = buf.slice(1,n+1).map(v=>{
+        v = v.split('')
+       return v
+   })
+   
 
 
-           }
-           if(same==false){
-            console.log("bu yong dao le")
-            console.log(str.join("\n").replace(/,/g,''))
-           }
-           
+    //判断对比
+
+    Array.prototype.equals = function (arr){
+        if(!arr){
+          return false;
+        }
+        if(this.length != arr.length) {
+          return false;
+        }
+        for(let i=0,l=arr.length;i<l;i++){
+          if(Array.isArray(this[i]) && Array.isArray(arr[i])){
+            if(!this[i].equals(arr[i])){
+              return false;
+            }
+          }else if(this[i] != arr[i]){
+            return false;
+          }
+        }
+        return true;
+      }
+if(str2.equals(dada))  {
+   var isequals = 10
+}  
+
+
+//判断输出
+    if(isequals == 10){
+        console.log("bu yong dao le")
+        console.log((str.join("\n").replace(/,/g,'').replace(/@/g,th)))
+       }else{
+        console.log((daoarr.join("\n").replace(/,/g,'').replace(/@/g,th)))
+       }
+            }
+         
+       
 });
 
--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-var str = "1,2,3,4"
-str = str.replace(/,/g,'')
-console.log(str)
+var arr = ["1","2","3","4","5"]
+var arr2 = ["1","2","3","4","5"]
+  for(var i = 0, j = 4 ;i<2,j>=3;i++,j--){
+       arr[i] = arr2[j]
+   arr[j] = arr2[i]
+}
+
+console.log(arr)
