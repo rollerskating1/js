96322ea8f53a3be8fb1fa1061cb54041bb36be93
rollerskating1 <837202942@qq.com>
2020-10-04 13:25:48 +0800

    seven

--- 7-22.js
+++ 7-22.js
@@ -1,4 +1,5 @@
 const { verify } = require('crypto');
+const { exit } = require('process');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -26,23 +27,62 @@ rl.on('line', (line) => {
 
 
 rl.on('close', () => {
-    var snum = Number(buf[0])
-    var len = buf.length
-    var endarr = len-1
-    if(len%2==0){
-        var cn = endarr/2
-    }
-    var arr = []
- for(var i = 0; i<len; i++){
-     var arrcontent = buf[i].toString().split(" ");
-     arr.push(arrcontent)
- }
-  arr.forEach(function(yuansu,xiabiao) {
-  for(var i = 0;i!=xiabiao;i++){
-     if(arr[0]==0){
+ 
+   
+   
+    var arr =[]
+    var sum = 0
+    for(var i = 0;i<buf.length;i++){
 
-     }
-  }
-  });
+arr.push(buf[i].toString().split(" "))
+
+    }
+ for(var k =1;k<=Number(buf[0]);k++){
+if( Number(arr[k][0])!==undefined){
+    sum += Number(arr[k][0])
+}
+    
+ }
+ let num = Number(buf[0])/2
+if(Number(buf[0])<=50 && Number(buf[0]) >0 && Number(buf[0])%2==0){
+    if(sum ==num){
+          for(var i = 1; i<=(Number(buf[0]))/2;i++){
+        if(arr !=undefined){
+            if(Number(arr[i][0])==0){
+for(var j =Number(buf[0]);j>=1;j--){
+    if(arr[i]!==undefined && arr[j]!==undefined){
+        if(arr[j][0]==1){
+        console.log(arr[i][1]+" "+arr[j][1])
+       delete arr[i]
+       delete arr[j]
+    //    console.log(arr)
+    }
+    }
+}
+        }else{
+ for(var j =Number(buf[0]);j>=1;j--){
+                if(arr[i]!==undefined && arr[j]!==undefined){
+                    if(arr[j][0]==0){
+                    console.log(arr[i][1]+" "+arr[j][1])
+                   delete arr[i]
+                   delete arr[j]
+                //    console.log(arr)
+                }
+                }
+            }
+        }
+       
+           
+                    
+    }else{
+        exit()
+    }
+        }
+// console.log(Number(arr[i][0]))
+
+    }
+  
+}
+    
 });
 
